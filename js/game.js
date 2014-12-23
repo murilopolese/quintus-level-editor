61bc1aff38d326cf6349961459ec6565ca448f78
Murilo Polese <murilopolese@gmail.com>
2014-12-23 04:05:27 +0100

    Init game canvas

--- js/game.js
+++ js/game.js
@@ -1,8 +1,11 @@
+
 $( document ).ready( function() {
 	var editor = Object.create( Editor );
 	$( '.create-game' ).on( 'click', function( e ) {
 		e.preventDefault();
+		// Fetch form data and set to object
 		editor.setOptions( $ );
+		// Create / Resize collision layer array
 		if( editor.collisionLayer.length ) {
 			editor.resizeCollisionLayerArray(
 				editor.level.width,
@@ -11,7 +14,45 @@ $( document ).ready( function() {
 		} else {
 			editor.updateCollisionLayerArray();
 		}
-		console.log( editor );
+		// Resize canvas element
+		$( '#game' ).attr({
+			'width': editor.level.width * editor.sprite.tileWidth + 'px',
+			'height': editor.level.height * editor.sprite.tileHeight + 'px'
+		});
+		// Fake initial sprite
+		editor.collisionLayer[ 0 ][ 0 ] = 0;
+		editor.collisionLayer[ 0 ][ 1 ] = 1;
+		editor.collisionLayer[ 0 ][ 2 ] = 2;
+		// Start editor
+		start( editor );
 		return false;
 	});
 });
+
+var start = function( editor ) {
+	var Q = window.Q = Quintus()
+	.include( "Sprites, Scenes, 2D" )
+	.setup( 'game', 
+		{
+			maximize: false,
+			imagePath: ""
+		}
+	);
+	Q.scene( "level1",function( stage ) {
+		console.log( 'level1' );
+		stage.collisionLayer( 
+			new Q.TileLayer({
+				dataAsset: editor.collisionLayer,
+				sheet: 'level'
+			}
+		));
+	});
+
+	Q.load( editor.sprite.url, function() {
+		Q.sheet( 
+			"level", editor.sprite.url, 
+			{ tilew: 32, tileh: 32 } 
+		);
+		Q.stageScene( "level1" );
+	});
+}
